dd14b55424d07d4b62cb5776e0e6db843f0fc3cb
parkilbeom <park1bum@naver.com>
2023-03-26 20:36:37 +0900

    [feat] cart temporary completion

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -40,96 +40,79 @@ const Desc = () => {
           placeholder={'쿠폰코드를 입력해주세요'}
           width={230}
           option={false}
+          border-radius={8}
         />
-        <button class="cupon_button">쿠폰적용</button>
+        <button className="cupon_button">쿠폰적용</button>
       </div>
-      <div>
-        <span>쿠폰/적립금은 주문서에서 사용가능합니다</span>
-        <a href="">계속 쇼핑하기</a>
+      <div className="buy_info">
+        <div className="bar"></div>
+        <div className="buy_text_box">
+          <span>쿠폰/적립금은 주문서에서 사용가능합니다</span>
+          <Link label="계속 쇼핑하기" path="/" option={3} />
+        </div>
       </div>
-      <Button label="구매하기 | 101,500원" heigth={56} />
+      <button className="buy_button">구매하기</button>
     </div>
   );
 };
 const Detail = ({ n, productPrice, productName }) => {
   const [counter, setCounter] = useState(n);
   return (
-    <>
-      <div className="detail-container">
-        <div className="detail-desc">
-          <Checkbox label=""></Checkbox>
-          <img src={card_img} className="product_img" alt="제품 썸네일" />
-          <span>{productName}</span>
-        </div>
-        <div className="detail-counter">
-          <button
-            className="counter-btn"
-            // onClick={() => setCounter(counter > 0 ? counter - 1 : 0)}
-            onClick={() =>
-              setCounter((counter) => (counter > 0 ? counter - 1 : 0))
-            }
-          >
-            -
-          </button>
-          <span className="current-number">{counter}</span>
+    <div className="detail-container">
+      <div className="detail-desc">
+        <Checkbox label=""></Checkbox>
+        <img src={card_img} className="product_img" alt="제품 썸네일" />
+        <span>{productName}</span>
+      </div>
+      <div className="detail-counter">
+        <button
+          className="counter-btn"
+          onClick={() =>
+            setCounter((counter) => (counter > 0 ? counter - 1 : 0))
+          }
+        >
+          -
+        </button>
+        <span className="current-number">{counter}</span>
 
-          <button
-            className="counter-btn"
-            //onClick={() => setCounter(counter + 1)} =>콜백함수에대해 이해해보자.
-            onClick={() => setCounter((counter) => counter + 1)}
-          >
-            +
-          </button>
-        </div>
-        <span>{productPrice}원</span>
-        <span>{counter * productPrice}원</span>
+        <button
+          className="counter-btn"
+          onClick={() => setCounter((counter) => counter + 1)}
+        >
+          +
+        </button>
       </div>
-      <div className="price_info">
-        <span className="total_Price_info">
-          상품금액 + 검수비 + 수수료 + 배송비
-        </span>
-        <span className="total_Price">총{counter * productPrice}원</span>
-      </div>
-      <section className="delivery_information">
-        <div className="delivery_box">
-          <h4>배송 안내</h4>
-          <div className="information_box">
-            <img src={cart_Icon01} alt="배송안내 아이콘" />
-            <p className="information_str">10시 이전 주문시 다음날 도착보장</p>
-            <p className="information_dsc">
-              주말과 공휴일에 따라 다를수 있음을 알려드립니다
-            </p>
-          </div>
-        </div>
-        <div className="delivery_box">
-          <h4>퀵서비스 안내</h4>
-          <div className="information_box">
-            <img src={cart_Icon02} alt="배송안내 아이콘" />
-            <p className="information_str">당일배송 서비스 가능</p>
-            <p className="information_dsc">
-              기념일 등 특별한 날에 빠르게 받아 보실 수 있습니다
-            </p>
-          </div>
-        </div>
-        <div className="delivery_box">
-          <h4>반품 안내</h4>
-          <div className="information_box">
-            <img src={cart_Icon03} alt="배송안내 아이콘" />
-            <p className="information_dsc">
-              30일 이내로 반품 신청해주셔야 반품 처리 진행 되오니 이점 유의해
-              주시기 바랍니다
-            </p>
-          </div>
-        </div>
-      </section>
-    </>
+      <span>{productPrice}원</span>
+      <span>{counter * productPrice}원</span>
+    </div>
   );
 };
 
 export const Cart = () => {
+  const cartStorage =
+    localStorage.getItem('cart') ??
+    localStorage.setItem(
+      'cart',
+      JSON.stringify([
+        {
+          productName: '몬스테라',
+          n: 1,
+          productPrice: 15000,
+        },
+        {
+          productName: '테이블야자',
+          n: 1,
+          productPrice: 25000,
+        },
+      ]),
+    );
+  const data = JSON.parse(cartStorage);
+  const [totalPrice, setTotalPrice] = useState(
+    data.reduce((acc, cur) => acc + cur.n * cur.productPrice, 0),
+  );
+  console.log(data[0]);
   return (
     <div>
-      <Header />
       <h2 className="cart_title">식집사의 장바구니</h2>
       <div className="cart_container">
         <div className="cart_detail">
@@ -137,7 +120,54 @@ export const Cart = () => {
             <h3 className="cart_my_order_title">나의 주문</h3>
             <span>{`(1)`}</span>
           </div>
-          <Detail n={0} productPrice={101500} productName={'몬스테라'} />
+          {data.map(({ productName, productPrice, n }, index) => (
+            <Detail
+              productName={productName}
+              productPrice={productPrice}
+              n={n}
+              key={index}
+            />
+          ))}
+          <div className="price_info">
+            <span className="total_Price_info">
+              상품금액 + 검수비 + 수수료 + 배송비
+            </span>
+            <span className="total_Price">총 {totalPrice}원</span>
+          </div>
+          <section className="delivery_information">
+            <div className="delivery_box">
+              <h4>배송 안내</h4>
+              <div className="information_box">
+                <img src={cart_Icon01} alt="배송안내 아이콘" />
+                <p className="information_str">
+                  10시 이전 주문시 다음날 도착보장
+                </p>
+                <p className="information_dsc">
+                  주말과 공휴일에 따라 다를수 있음을 알려드립니다
+                </p>
+              </div>
+            </div>
+            <div className="delivery_box">
+              <h4>퀵서비스 안내</h4>
+              <div className="information_box">
+                <img src={cart_Icon02} alt="배송안내 아이콘" />
+                <p className="information_str">당일배송 서비스 가능</p>
+                <p className="information_dsc">
+                  기념일 등 특별한 날에 빠르게 받아 보실 수 있습니다
+                </p>
+              </div>
+            </div>
+            <div className="delivery_box">
+              <h4>반품 안내</h4>
+              <div className="information_box">
+                <img src={cart_Icon03} alt="배송안내 아이콘" />
+                <p className="information_dsc">
+                  30일 이내로 반품 신청해주셔야 반품 처리 진행 되오니 이점
+                  유의해 주시기 바랍니다
+                </p>
+              </div>
+            </div>
+          </section>
         </div>
         <Desc />
       </div>
