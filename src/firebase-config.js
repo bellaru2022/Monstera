dfce92403ca23296dfc0898959f7cd15e70fa6d1
parkilbeom <park1bum@naver.com>
2023-03-28 23:01:26 +0900

    [comment] modify firebase config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -13,25 +13,25 @@ const {
   VITE_FIREBASE_MEASUREMENT_ID,
 } = import.meta.env;
 
-// const firebaseConfig = {
-//   apiKey: VITE_FIREBASE_API_KEY,
-//   authDomain: VITE_FIREBASE_AUTH_DOMAIN,
-//   projectId: VITE_FIREBASE_PROJECT_ID,
-//   storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: VITE_FIREBASE_SENDER_ID,
-//   appId: VITE_FIREBASE_APP_ID,
-//   measurementId: VITE_FIREBASE_MEASUREMENT_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: 'AIzaSyAC8hWd3U77OPAJLMvzK-EftUJKj_J7l98',
-  authDomain: 'monstera-ad980.firebaseapp.com',
-  projectId: 'monstera-ad980',
-  storageBucket: 'monstera-ad980.appspot.com',
-  messagingSenderId: '280388407086',
-  appId: '1:280388407086:web:fe12edb774e31e6fc274bf',
-  measurementId: 'G-MJ29RQ4MTK',
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: VITE_FIREBASE_PROJECT_ID,
+  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_SENDER_ID,
+  appId: VITE_FIREBASE_APP_ID,
+  measurementId: VITE_FIREBASE_MEASUREMENT_ID,
 };
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyAC8hWd3U77OPAJLMvzK-EftUJKj_J7l98',
+//   authDomain: 'monstera-ad980.firebaseapp.com',
+//   projectId: 'monstera-ad980',
+//   storageBucket: 'monstera-ad980.appspot.com',
+//   messagingSenderId: '280388407086',
+//   appId: '1:280388407086:web:fe12edb774e31e6fc274bf',
+//   measurementId: 'G-MJ29RQ4MTK',
+// };
 
 const app = firebase.initializeApp(firebaseConfig);
 
